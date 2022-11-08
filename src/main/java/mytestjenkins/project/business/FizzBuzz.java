f699394ff58ea81139a495ce270ab3d4d9e26d87
AndRog <rognetta.andrea@yahoo.com>
2022-11-08 19:05:37 +0100

    try jenkins

--- src/main/java/mytestjenkins/project/business/FizzBuzz.java
+++ src/main/java/mytestjenkins/project/business/FizzBuzz.java
@@ -3,10 +3,7 @@ package mytestjenkins.project.business;
 public class FizzBuzz {
 
     public String play(int number) {
-    	
-    	
-    	
-    	
+    	System.out.println("ciao");
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
